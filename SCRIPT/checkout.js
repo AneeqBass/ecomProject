7e68acd78ab9b94687fb8894ce5c28b842a36cd2
AneeqBass <aneeqbass@gmail.com>
2024-06-19 16:30:46 +0200

    added products and cart function

--- SCRIPT/checkout.js
+++ SCRIPT/checkout.js
@@ -1,48 +1,45 @@
-const cart = JSON.parse(localStorage.getItem('cart'));
-const cartItemsContainer = document.getElementById('cartItems');
-const totalPriceElement = document.getElementById('totalPrice');
-const payButton = document.getElementById('payButton');
-const clearCartButton = document.getElementById('clearCartButton');
+let cart = JSON.parse(localStorage.getItem('cart'));
+let cartItemsContainer = document.getElementById('cartItems');
+let totalPriceElement = document.getElementById('totalPrice');
+let payButton = document.getElementById('payButton');
+let clearCartButton = document.getElementById('clearCartButton');
 
 function populateCart() {
     let cartContent = '';
     let totalPrice = 0;
 
     cart.forEach(product => {
-        cartContent += `
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="${product.image}" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">${product.name}</h5>
-                        <p class="card-text">Quantity: ${product.quantity}</p>
-                        <p class="card-text">Price: R${product.price}</p>
-                    </div>
-                </div>
+        cartContent +=`
+        <div class="col-md-4 mb-4">
+            <div class="card">
+            <img src="${product.image}" class="card-img-top" alt="${product.name}">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <h3 class="price">${product.price.toFixed(2)}</h3>
+                <p class="card-text">${product.category}</p>
+                <button class="btn btn-dark addToCart" data-id="${product.id}">Add To Cart</button>
             </div>
+            </div>
+        </div>
         `;
-        totalPrice += product.price * product.quantity;
+        totalPrice += product.price
     });
 
     cartItemsContainer.innerHTML = cartContent;
     totalPriceElement.textContent = totalPrice.toFixed(2);
 }
+populateCart();
 
 function clearCart() {
     localStorage.removeItem('cart');
     cartItemsContainer.innerHTML = '';
     totalPriceElement.textContent = '0.00';
 }
+clearCartButton.addEventListener('click', clearCart);
 
 
-function handlePayment() {
-    alert('Your payment successful! Thank you for shopping at RETRO KITZ'); 
+function pay() {
+    alert('Your payment successful! Thank you for shopping at '); 
     clearCart();
 }
-
-    
-populateCart();
-
-    
-clearCartButton.addEventListener('click', clearCart);
-payButton.addEventListener('click', handlePayment);
+payButton.addEventListener('click', pay);
